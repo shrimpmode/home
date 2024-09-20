de81d74609624166082e19b85818320ab7587bc9
shrimpmode <jsilva@uni.pe>
2024-09-20 12:57:55 -0500

    change bio color

--- src/app/components/bio/BioSection.tsx
+++ src/app/components/bio/BioSection.tsx
@@ -1,18 +1,18 @@
-import {Section} from "@/app/components/section/Section";
-import {LeftSection} from "@/app/components/section/LeftSection";
-import {Content} from "@/app/components/bio/Content";
+import { Section } from "@/app/components/section/Section";
+import { LeftSection } from "@/app/components/section/LeftSection";
+import { Content } from "@/app/components/bio/Content";
 
 export const BioSection = () => {
-    return (
-        <Section
-            leftSection={
-                <LeftSection
-                    bg='#FFEFCD'
-                    title={<div className='text-neutral-900'>Bio</div>}
-                />
-        }
-            >
-            <Content />
-        </Section>
-    )
+  return (
+    <Section
+      leftSection={
+        <LeftSection
+          bg='#C9E4DE'
+          title={<div className='text-neutral-900'>Bio</div>}
+        />
+      }
+    >
+      <Content />
+    </Section>
+  )
 }
